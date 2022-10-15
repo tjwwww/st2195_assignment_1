b70b941fc7a6a83e015b43ba315fdcd5c8995986
tjwwww <jwtham002@mymail.sim.edu.sg>
2022-10-15 23:26:42 +0800

    adding python files

---
+++ spyder/numbers_python.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 15 15:13:18 2022
+
+@author: WIN11-PC
+"""
+
+# Print integers 1 to 10 without loop
+
+print(1,2,3,4,5,6,7,8,9,10)
+
+# Print integers 1 to 10 with loop
+
+for i in range (1,11) :
+    print(i)
+
+for i in range (1,11) :
+    print(i, end=" ")
+
+x = 1
+while x <= 11:
+    print(x)
+    x = x + 1
